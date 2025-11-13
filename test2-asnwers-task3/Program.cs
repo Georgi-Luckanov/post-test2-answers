44c935a5968be625760a9abbb41827d4bf05d09c
Georgi-Luckanov <et.georgi.l@eduiteh.eu>
2025-11-13 10:18:20 +0200

    Test 2 answers task 3

---
+++ test2-asnwers-task3/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int num = int.Parse(Console.ReadLine());
+if (num < 0)
+    Console.WriteLine("Error");
+else
+{
+    double sum = 1;
+    if (num == 0)
+    {
+        Console.WriteLine(1);
+    }
+    else
+    {
+        int product = 1;
+        for (int i = num; i > 0; i--)
+        {
+            product = product * i;
+        }
+        Console.WriteLine(sum);
+    }
+}
