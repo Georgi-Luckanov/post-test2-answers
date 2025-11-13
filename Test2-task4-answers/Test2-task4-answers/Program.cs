8b9541d277a72f0bf71edd4a6383b456d48d9717
Georgi-Luckanov <et.georgi.l@eduiteh.eu>
2025-11-13 10:58:50 +0200

    test2 task4 answers

---
+++ Test2-task4-answers/Test2-task4-answers/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int num = 14;
+int guess;
+do
+{
+    guess = int.Parse(Console.ReadLine());
+    if (guess > num)
+    {
+        Console.WriteLine("Your guess is higher");
+    }
+    else if (guess < num)
+    {
+        Console.WriteLine("Your guess is lower");
+    }
+} while (guess != num);
+Console.WriteLine("You win");
