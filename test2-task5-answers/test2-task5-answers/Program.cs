6f2f0c0473479550521c97c560e32daa9f94fc5a
Georgi-Luckanov <et.georgi.l@eduiteh.eu>
2025-11-17 09:20:19 +0200

    post test 2 asnwers
    something post test

---
+++ test2-task5-answers/test2-task5-answers/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+
+Console.WriteLine("Hello, World!");
+int num = int.Parse(Console.ReadLine());
+if (num > 0)
+{
+    String dividers = "";
+    for (int i = 1; i < num; i++)
+    {
+
+        if (num % i == 0)
+        {
+            dividers += " ";
+        }
+        if
+
+
+
+    }
+}
+else
+{
+    Console.WriteLine("Negative")
+}
+//dovueshi zadachata 
